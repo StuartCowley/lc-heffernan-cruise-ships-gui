276ada579821a22471e366cda2a01733a20c28a5
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-28 21:04:59 +0100

    created setSail method on controller and a button that when clicked sails the ship to the next port on the itinerary

--- src/Controller.js
+++ src/Controller.js
@@ -1,7 +1,13 @@
 (function exportController(){
     class Controller {
-        constructor() {
+        constructor(ship) {
+            this.ship = ship;
             this.initialiseSea();
+
+            const sailButton = document.querySelector('#sailbutton');
+            sailButton.addEventListener('click', () => {
+                this.setSail();
+            });
         }
         initialiseSea() {
             const backgrounds = [
@@ -28,15 +34,40 @@
                 portsElement.style.width = `${portsElementWidth + 256}px`;
             });
         };
-        renderShip(ship){
+        renderShip(){
+            const ship = this.ship;
             console.log("renderShip");
             const indexOfCurrentPort = ship.itinerary.ports.indexOf(ship.currentPort);
             const currentPortElement = document.querySelector(`[data-port-index="${indexOfCurrentPort}"]`);
             const shipElement = document.querySelector('#ship');
             shipElement.style.top = `${currentPortElement.offsetTop + 15}px`;
             shipElement.style.left = `${currentPortElement.offsetLeft - 8}px`;
-            // currentPortElement.appendChild(shipElement);
         };
+        setSail() {
+            const ship = this.ship;
+            const indexOfNextPort = ship.itinerary.ports.indexOf(ship.currentPort) + 1;
+            const nextPortElement = document.querySelector(`[data-port-index="${indexOfNextPort}"]`);
+            if (!nextPortElement){
+                return alert('End of the line!');
+            } 
+            const shipElement = document.querySelector('#ship');
+            const end = parseInt(nextPortElement.offsetLeft,10);
+            
+            let sailInterval = null;
+            sailInterval = setInterval(frame, 20);
+            function frame() {
+                let posX = parseInt(shipElement.style.left, 10);
+                if(posX === (end - 8)){
+                    ship.setSail();
+                    ship.dock();
+                    clearInterval(sailInterval);
+                } else {
+                    shipElement.style.left = `${posX + 1}px`;   
+                }
+               
+            };
+            
+        }
     };
     if(typeof module !== 'undefined' && module.exports){
         module.exports = Controller;
